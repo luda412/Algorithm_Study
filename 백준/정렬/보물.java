165463fa99498dfda3e0b6292f6c6091da04095c
lee <81570208+luda412@users.noreply.github.com>
2024-09-24 14:15:23 +0900

    그리디 | 1026 | 최소값

---
+++ 백준/정렬/보물.java
@@ -0,0 +1,37 @@
+package 백준.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class 보물 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        //내림차순 정렬을 하려면 int가 아닌 Integer로 선언
+        Integer[] B = new Integer[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            B[i] = Integer.parseInt(st2.nextToken());
+        }
+        //A 올림, B 내림
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += (A[i] * B[i]);
+        }
+        System.out.println(sum);
+
+    }
+}
